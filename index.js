38af8f77e0232c8fa48deefabe4c1a4bdb93ca49
Roman Pilipovskiy <r.pilipovskiy@vilion-k.ru>
2023-05-26 22:23:52 +0300

    08-EJS Homework

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('./middleware/logger');
+const error404 = require('./middleware/error404');
+const indexRouter = require('./routes/index');
+const booksRouter = require('./routes/books');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.set("view engine", "ejs");
+
+app.use(logger);
+
+app.use('/', indexRouter);
+app.use('/books', booksRouter);
+
+app.use(error404);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
